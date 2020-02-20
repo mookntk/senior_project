b57297bf8036d2379209f8de18e0e99a92596cda
muk378 <muk7859@hotmail.com>
2020-02-20 11:17:58 +0700

    add ready_sell.js

---
+++ nodeserver/routes/ready_sell.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const router = express.Router();
+const {
+    check
+} = require("express-validator");
+const db = require("../configs/db");
+const ready_sell = "ready_sell";
+
+//show patient order
+router.post(
+    "/ready_order",
+    [
+        check("pharmacy_id")
+        .not()
+        .isEmpty()
+    ],
+    async (req, res) => {
+        try {
+            const r_order = await ready_order(req.body);
+            res.json(r_order);
+        } catch (error) {
+            res.status(400).json({
+                message: error.message
+            });
+        }
+    }
+);
+
+var ready_order = function (item) {
+    return new Promise((resolve, reject) => {
+        db.query(
+            "SELECT o.order_id,o.patient_HN_order,p.name,p.surname,o.status,DATE_FORMAT(o.due_date,'%Y %m %d') AS due_date " +
+            "from orders as o " +
+            "left join patients as p ON o.patient_HN_order = p.patient_HN " +
+            "WHERE o.status ='ready' " +
+            "AND o.pharmacy_id ='" +
+            item.pharmacy_id +
+            "'" +
+            "group by o.order_id",
+            (error, result) => {
+                if (error) return reject(error);
+                resolve(result);
+            }
+        );
+    });
+};
+
+//oneorder gor each patient
+router.post(
+    "/one_order",
+    // [
+    //     check("pharmacy_id")
+    //     .not()
+    //     .isEmpty()
+    // ],
+    [
+        check("patient_HN_order")
+        .not()
+        .isEmpty()
+    ],
+    async (req, res) => {
+        try {
+            const oneorder = await one_order(req.body);
+            res.json(oneorder);
+        } catch (error) {
+            res.status(400).json({
+                message: error.message
+            });
+        }
+    }
+);
+
+var one_order = function (item) {
+    return new Promise((resolve, reject) => {
+        db.query(
+            "SELECT o.order_id,p.name,p.surname,p.gender,p.DOB,o.status,o.patient_HN_order,m.medicine_generic,m.strenght,od.qty,m.unit " +
+            // "group_concat(m.medicine_generic) as medicine_generic, " +
+            // "group_concat(m.strenght) as strenght, " +
+            // "group_concat(od.qty) as qty, " +
+            // "group_concat(m.unit) as unit " +
+            "from orders AS o "+
+            "left join patients as p ON o.patient_HN_order = p.patient_HN "+
+            "left join order_detail as od on od.order_id = o.order_id "+
+            "inner join medicine as m ON m.medicine_id = od.medicine_id " +
+            "WHERE o.status ='ready' " +
+            // "AND o.pharmacy_id ='" +
+            // item.pharmacy_id +
+            // "'" +
+            "AND o.patient_HN_order ='" +
+            item.patient_HN_order +
+            "'" ,
+            // +
+            // "group by o.order_id",
+            (error, result) => {
+                if (error) return reject(error);
+                resolve(result);
+            }
+        );
+    });
+};
+
+module.exports = router;
